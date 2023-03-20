540801740f1d915bc8d442e778a1b5a20d74e628
jliangrpb <jeremy.liang612@gmail.com>
2023-03-20 16:44:30 -0400

    Add reject/any?/all?

--- enum.rb
+++ enum.rb
@@ -16,4 +16,25 @@ class Array
         new_arr
     end
 
+    def my_reject(&blc)
+        result = []
+            self.my_each { |ele| result << ele if blc[ele] == false }
+        result 
+    end
+
+    def my_any?(&blc)
+        self.my_each {|ele| return true if blc[ele]}
+        false
+    end
+
+    def my_all?(&blc)
+        self.my_each {|ele| return false if !blc[ele]}
+        true
+    end
 end
+
+a = [1, 2, 3]
+p a.my_any? { |num| num > 1 } # => true
+p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
