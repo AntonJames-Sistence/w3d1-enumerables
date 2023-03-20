33bf4acda5bf77603dd445a2ce7f28d2e614ea17
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-03-20 16:35:49 -0400

    add my_select

--- enum.rb
+++ enum.rb
@@ -1,11 +1,19 @@
 class Array
-    def my_each(&prc)
+    def my_each(&blc) # { |ele| new_arr << ele if blc[ele] == true }
         i = 0
         while i < self.length
-            prc.call(self[i])
+            blc.call(self[i])
             i+=1
         end
         self
+    end
+
+    def my_select(&blc) # suppose to take a block with boolean 
+        new_arr = []
+
+        self.my_each { |ele| new_arr << ele if blc[ele] }
+
+        new_arr
     end
 
 end
